3ea06520d8f38c932ce205fedb9eb13ed8e484b6
2seunghye <tmdgp970120@naver.com>
2021-01-06 09:36:40 +0900

    Router 수정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import './App.css';
+import WordBookPage from './routes/WordBookPage';
+import WordTestPage from './routes/WordTestPage';
 import Gnb from './Components/Navigation/Gnb';
 
+import SpellingTest from './Components/WordTest/spellingTest/SpellingTest';
+import ChoiceTest from './Components/WordTest/choiceTest/ChoiceTest';
+
+import { HashRouter, Route } from 'react-router-dom';
+
 function App() {
-  return <Gnb />;
+  return (
+    <HashRouter>
+      <Gnb />
+      <HashRouter>
+        <Route path="/" exact={true} component={WordBookPage} />
+        <Route path="/test" exact={true} component={WordTestPage} />
+        <Route path="/test/spellingTest" component={SpellingTest} />
+        <Route path="/test/choiceTest" component={ChoiceTest} />
+      </HashRouter>
+    </HashRouter>
+  );
 }
 
 export default App;
