6980b3974426d130f711628926d73fd8024b55ad
2seunghye <tmdgp970120@naver.com>
2021-01-21 21:23:51 +0900

    Refactoring WordTestPage.js

--- src/routes/WordTestPage.js
+++ src/routes/WordTestPage.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import TestMainPage from '../Components/WordTest/testHome/TestMainPage';
 
-class WordTestPage extends React.Component {
-  render() {
-    return (
-      <div>
-        <TestMainPage />
-      </div>
-    );
-  }
-}
+const WordTestPage = () => {
+  return (
+    <div>
+      <TestMainPage />
+    </div>
+  );
+};
 
 export default WordTestPage;
