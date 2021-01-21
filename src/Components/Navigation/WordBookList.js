79dadc191f3495d402887bd292f6d16614bdd4e7
2seunghye <tmdgp970120@naver.com>
2021-01-21 22:26:46 +0900

    Refactoring WordBookList.js

--- src/Components/Navigation/WordBookList.js
+++ src/Components/Navigation/WordBookList.js
@@ -1,80 +1,72 @@
-import React from 'react';
 import UserWordBookList from './UserWordBookList';
 import GroupList from './GroupList';
 import './WordBookList.css';
 
-class WordBookList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      wordBookList: {
-        userWordBookList: [
+const WordBookList = () => {
+  const wordBookList = {
+    userWordBookList: [
+      {
+        wordBookId: '단어장1',
+      },
+      {
+        wordBookId: '단어장2',
+      },
+      {
+        wordBookId: '단어장3',
+      },
+    ],
+    groupList: [
+      {
+        groupName: 'Group1',
+        groupWordBookList: [
           {
-            wordBookId: '단어장1',
+            wordBookId: '그룹단어장1',
           },
           {
-            wordBookId: '단어장2',
+            wordBookId: '그룹단어장2',
           },
           {
-            wordBookId: '단어장3',
-          },
-        ],
-        groupList: [
-          {
-            groupName: 'Group1',
-            groupWordBookList: [
-              {
-                wordBookId: '그룹단어장1',
-              },
-              {
-                wordBookId: '그룹단어장2',
-              },
-              {
-                wordBookId: '그룹단어장3',
-              },
-            ],
-          },
-          {
-            groupName: 'Group2',
-            groupWordBookList: [
-              {
-                wordBookId: '그룹단어장1',
-              },
-              {
-                wordBookId: '그룹단어장2',
-              },
-              {
-                wordBookId: '그룹단어장3',
-              },
-            ],
-          },
-          {
-            groupName: 'Group3',
-            groupWordBookList: [
-              {
-                wordBookId: '그룹단어장1',
-              },
-              {
-                wordBookId: '그룹단어장2',
-              },
-              {
-                wordBookId: '그룹단어장3',
-              },
-            ],
+            wordBookId: '그룹단어장3',
           },
         ],
       },
-    };
-  }
+      {
+        groupName: 'Group2',
+        groupWordBookList: [
+          {
+            wordBookId: '그룹단어장1',
+          },
+          {
+            wordBookId: '그룹단어장2',
+          },
+          {
+            wordBookId: '그룹단어장3',
+          },
+        ],
+      },
+      {
+        groupName: 'Group3',
+        groupWordBookList: [
+          {
+            wordBookId: '그룹단어장1',
+          },
+          {
+            wordBookId: '그룹단어장2',
+          },
+          {
+            wordBookId: '그룹단어장3',
+          },
+        ],
+      },
+    ],
+  };
 
-  render() {
-    return (
-      <div className="WordBookList">
-        <UserWordBookList userWordBookList={this.state.wordBookList.userWordBookList} />
-        <GroupList groupList={this.state.wordBookList.groupList} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="WordBookList">
+      <UserWordBookList userWordBookList={wordBookList.userWordBookList} />
+      <GroupList groupList={wordBookList.groupList} />
+    </div>
+  );
+};
 
 export default WordBookList;
