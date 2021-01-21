0ccf2fd6122b7c3cce88a21593d8bf3fdd485214
2seunghye <tmdgp970120@naver.com>
2021-01-21 22:05:07 +0900

    Refactoring GroupList.js

--- src/Components/Navigation/GroupList.js
+++ src/Components/Navigation/GroupList.js
@@ -2,14 +2,12 @@ import React, { Component } from 'react';
 import Group from './Group.js';
 import './GroupList.css';
 
-class GroupList extends Component {
-  render() {
-    const groupList = this.props.groupList;
-    const groups = groupList.map((group, index) => {
-      return <Group key={index} group={group} />;
-    });
-    return <div className="groups">{groups}</div>;
-  }
-}
+const GroupList = (props) => {
+  const groupList = props.groupList;
+  const groups = groupList.map((group, index) => {
+    return <Group key={index} group={group} />;
+  });
+  return <div className="groups">{groups}</div>;
+};
 
 export default GroupList;
