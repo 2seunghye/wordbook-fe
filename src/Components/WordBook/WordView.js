078c52a368a93515990a18193e3783aed1a19487
2seunghye <tmdgp970120@naver.com>
2021-01-21 23:01:58 +0900

    Refactoring WordView.js

--- src/Components/WordBook/WordView.js
+++ src/Components/WordBook/WordView.js
@@ -1,41 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './WordView.css';
 
-class WordView extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { isOpen: this.props.word.isOpen };
-  }
+const WordView = (props) => {
+  const [isOpen, setIsOpen] = useState(props.word.isOpen);
+  const { voca, meaning, id } = props.word;
+  const { onRemoveWord, onToggleIsBeenModifying } = props;
 
-  handleClick = () => {
-    this.setState((state) => ({
-      isOpen: !state.isOpen,
-    }));
+  const handleClick = () => {
+    setIsOpen(!isOpen);
   };
 
-  render() {
-    const { voca, meaning, id } = this.props.word;
-    const { onRemoveWord } = this.props;
-    const { isOpen } = this.state;
-    return (
-      <div className="WordView">
-        <div className="word-card" onClick={this.handleClick}>
-          <div className="voca">
-            <span>{voca}</span>
-          </div>
-          <div className={isOpen ? 'meaning' : 'meaning hidden'}>
-            <span>{meaning}</span>
-          </div>
+  return (
+    <div className="WordView">
+      <div className="word-card" onClick={handleClick}>
+        <div className="voca">
+          <span>{voca}</span>
         </div>
-        <button type="button" onClick={() => onRemoveWord(id)}>
-          삭제버튼
-        </button>
-        <button type="button" onClick={this.props.onToggleIsBeenModifying}>
-          수정버튼
-        </button>
+        <div className={isOpen ? 'meaning' : 'meaning hidden'}>
+          <span>{meaning}</span>
+        </div>
       </div>
-    );
-  }
-}
+      <button type="button" onClick={() => onRemoveWord(id)}>
+        삭제버튼
+      </button>
+      <button type="button" onClick={onToggleIsBeenModifying}>
+        수정버튼
+      </button>
+    </div>
+  );
+};
 
 export default WordView;
