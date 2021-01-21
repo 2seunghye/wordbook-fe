ab65b4b5a1912e1dd8ea43eec1a59d57ea2badab
2seunghye <tmdgp970120@naver.com>
2021-01-21 22:54:36 +0900

    Refactoring Word.js

--- src/Components/WordBook/Word.js
+++ src/Components/WordBook/Word.js
@@ -1,36 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Word.css';
 import WordView from './WordView';
 import ModifyWord from './ModifyWord';
 
-class Word extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isBeenModifying: false,
-    };
-  }
+const Word = (props) => {
+  const [isBeenModifying, setIsBeenModifying] = useState(false);
 
-  onToggleIsBeenModifying = () => {
-    this.setState((state) => ({
-      isBeenModifying: !state.isBeenModifying,
-    }));
+  const onToggleIsBeenModifying = () => {
+    setIsBeenModifying(!isBeenModifying);
   };
 
-  render() {
-    const { word, onReviseWord, onRemoveWord } = this.props;
-    const { isBeenModifying } = this.state;
+  const word = props.word;
+  const onReviseWord = props.onReviseWord;
+  const onRemoveWord = props.onRemoveWord;
 
-    return (
-      <div className="Word">
-        {isBeenModifying ? (
-          <ModifyWord onToggleIsBeenModifying={this.onToggleIsBeenModifying} word={word} onReviseWord={onReviseWord} />
-        ) : (
-          <WordView onToggleIsBeenModifying={this.onToggleIsBeenModifying} word={word} onRemoveWord={onRemoveWord} />
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="Word">
+      {isBeenModifying ? (
+        <ModifyWord onToggleIsBeenModifying={onToggleIsBeenModifying} word={word} onReviseWord={onReviseWord} />
+      ) : (
+        <WordView onToggleIsBeenModifying={onToggleIsBeenModifying} word={word} onRemoveWord={onRemoveWord} />
+      )}
+    </div>
+  );
+};
 
 export default Word;
